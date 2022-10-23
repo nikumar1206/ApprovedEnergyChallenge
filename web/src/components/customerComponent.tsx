13d7abd69d96cdbb5be82dd1aa8f61d2ef7a2642
Nikhil Kumar <nikumar1206@gmail.com>
2022-10-23 01:54:16 -0400

    started setup for fetching customers and some frontend cleaning

--- web/src/components/customerComponent.tsx
+++ web/src/components/customerComponent.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
+import { fetchAllCustomers } from "../utils/customer_api";
+
 const CustomerComponent = () => {
+	const [data, setData] = useState(null);
+	useEffect(() => {
+		fetchAllCustomers().then((res) => setData(res!.data));
+	}, []);
 	return (
 		<>
-			<h1>hllo this is the customer component</h1>
+			<h1>{JSON.stringify(data, null, 2)}</h1>
 		</>
 	);
 };
---
+++ web/src/utils/product_api.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+interface productData {
+	name: string;
+	type: string;
+	price: string;
+	expiration: Date;
+}
+
+export const fetchAllproducts = async () => {
+	try {
+		const data = await axios.get("/api/products");
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const fetchProduct = async (id: number) => {
+	try {
+		const data = await axios.get(`/api/products/${id}`);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const createProduct = async (product: productData) => {
+	try {
+		const data = await axios.post(`/api/products/`, product);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updateProduct = async (id: number, product: productData) => {
+	try {
+		const data = await axios.post(`/api/products/${id}`, product);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const deleteProduct = async (id: number) => {
+	try {
+		const data = await axios.post(`/api/products/${id}`);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
